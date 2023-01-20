76a6ce025174eaf5c98b53564c70bc25585097ed
HanKyeon <hgh21233@gmail.com>
2023-01-20 23:11:24 +0900

    [FE] Feat: 대각선 파도 (#52)
    1. 대각선 파도 추가
    2. wave-container 논클릭 속성들 추가

--- frontend/src/pages/TestPage.jsx
+++ frontend/src/pages/TestPage.jsx
@@ -1,4 +1,5 @@
 // import SecondWave from "../components/Canvas/SecondWave/SecondWave"
+import DiagonalWave from "../components/Canvas/DiagonalWave/DiagonalWave"
 import Wave from "../components/Canvas/Wave/Wave"
 
 function TestPage() {
@@ -6,8 +7,12 @@ function TestPage() {
     <>
       <div>테스트</div>
       <div>테스트</div>
-      <div>테스트</div>
-      <div>테스트</div>
+      <DiagonalWave
+        canvasHeight={900}
+        canvasWidth={1300}
+        leftColor={`rgba(255, 255, 255, 1)`}
+        rightColor={`rgba(29, 88, 164, 0.66)`}
+      />
       <Wave canvasHeight={1000} canvasWidth={1200} />
     </>
   )
