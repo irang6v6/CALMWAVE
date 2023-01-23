46fa197a6446d340815582f2d1b9fdb0245b6c14
HanKyeon <hgh21233@gmail.com>
2023-01-23 23:56:21 +0900

    [FE] Feat: 404 페이지 대충 만들어둠 (#78)
    1. 404 페이지 대충 만들어둠.
    2. Ref로 변경될 때 window.innerHeight 이런 것들이 바뀌는 것으로
       useEffect를 썼기에 크기가 바뀔 때마다 별 위치가 재조정 되는데 어떻게
       막을 방법이 없는거 같음.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ProfilePage from "./pages/Profile/ProfilePage"
 import RoomResult from "./pages/Room/RoomResult/RoomResult"
 import SignParentPage from "./pages/Sign/SignParentPage"
 import TestPage from "./pages/TestPage"
+import NotFound from "./pages/NotFound/NotFound"
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path={`/profile`} element={<ProfilePage />} />
         <Route path={`/profile/:infoType`} element={<ProfilePage />} />
         <Route path={`test`} element={<TestPage />} />
+        <Route path={`*`} element={<NotFound />} />
       </Routes>
     </div>
   )
