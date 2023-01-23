3ffd5891ad865ee9e1075eb30781dc1b7a96388e
haneejo <haandeng@naver.com>
2023-01-23 16:45:16 +0900

    [FE] Feat: Home 페이지
    코드 인덴트 맞추기

--- frontend/src/pages/Home/NavBar.jsx
+++ frontend/src/pages/Home/NavBar.jsx
@@ -5,18 +5,22 @@ import logoImg from "../../assets/calmwave.png"
 function NavBar() {
   return (
     <header>
-        <div className={`${styles["logo-box"]}`}>
-        <a href="#none"><img src={logoImg} className={`${styles["logo-img"]}`} alt="calmwave-logo"/></a>
-        </div>
+      <div className={`${styles["logo-box"]}`}>
+        <a href="#none">
+          <img
+            src={logoImg}
+            className={`${styles["logo-img"]}`}
+            alt="calmwave-logo"
+          />
+        </a>
+      </div>
 
-        <div className={`${styles["menu-box"]}`}>
-            <a href="#none">HOME</a>
-            <a href="#none">LOGIN</a>
-            <a href="#none">SIGNUP</a>
-        </div>
-
+      <div className={`${styles["menu-box"]}`}>
+        <a href="#none">HOME</a>
+        <a href="#none">LOGIN</a>
+        <a href="#none">SIGNUP</a>
+      </div>
     </header>
   )
 }
 export default NavBar
- 
--- frontend/src/pages/Profile/Router4/Router4.js
+++ frontend/src/pages/Profile/Router4/Router4.js
@@ -1,14 +1,13 @@
-import './Router4.css';
-
+import "./Router4.css"
 
 const Router4 = () => {
-    return (
-        <div className="routecontainer4">
-            <div className="infoBox4">
-                <h3> 4번 페이지입니다. </h3>
-            </div>
-        </div>
-    )
+  return (
+    <div className="routecontainer4">
+      <div className="infoBox4">
+        <h3> 4번 페이지입니다. </h3>
+      </div>
+    </div>
+  )
 }
 
 export default Router4
