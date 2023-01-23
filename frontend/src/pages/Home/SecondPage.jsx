36b4f1cad276918af364c7d958147042c6f3f5ca
haneejo <haandeng@naver.com>
2023-01-23 13:58:06 +0900

    [FE] Feat: Home 페이지(#83)
    홈 페이지 구조 잡기
    이미지 및 기능 설명은 전부 임시.
    반응형 적용 X

---
+++ frontend/src/pages/Home/SecondPage.jsx
@@ -0,0 +1,14 @@
+import styles from "./SecondPage.module.css"
+import React from "react"
+
+function SecondPage() {
+  return (
+    <div className={`${styles["container"]}`}>
+        <h2>내 모든 업무 현황을 한눈에 조회하고 한 곳에서 관리하세요.<br/>
+        이제껏 경험 못 했던 쉽고 편리한 금융 서비스,<br/>
+        calmwave와 함께라면 당신의 일상이 새로워질 거예요.</h2>
+    </div>
+  )
+}
+export default SecondPage
+ 
