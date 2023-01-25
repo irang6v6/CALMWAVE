f4dc914e83bf2346f4c24c0df2ff8ccddcb34f2a
HanKyeon <hgh21233@gmail.com>
2023-01-25 22:59:36 +0900

    [FE] Fix: RTK conflict
    RTK 컨플릭트 난듯. index에 올려둔 user가 사라져서 업데이트.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
-import exReducer from "./ex-slice"
+// import exReducer from "./ex-slice"
 import todosSlice from "./todos-slice"
+import userSlice from "./user-slice"
 
 const store = configureStore({
   reducer: {
-    ex: exReducer,
+    user: userSlice,
     todos: todosSlice,
   },
 })
