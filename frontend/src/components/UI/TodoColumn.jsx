7781a434467bf93b8e82b54eb69eb18ccc0c7477
SeoYong <swqq31@gmail.com>
2023-02-01 17:56:49 +0900

    [FE] Feat: 룸페이지 DnD (#177)

--- frontend/src/components/UI/TodoColumn.jsx
+++ frontend/src/components/UI/TodoColumn.jsx
@@ -1,16 +1,50 @@
 import { useDrop } from "react-dnd"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import styles from "./TodoColumn.module.css"
+import { todoActions } from "../../store/todos-slice"
+
 export default function TodoColumn({ children, className, title }) {
+  const todos = useSelector((state) => state.todos.todos)
   const progress = useSelector((state) => state.todos.onProgress)
+
+  const dispatch = useDispatch()
 
   const canMoveTodo = (title) => {
     return title !== "In Progress" || !progress
   }
+
+  const moveCardColumnHandler = (dragItem, hoverColumn) => {
+    const prevState = [...todos]
+    dispatch(
+      todoActions.changeTodos(
+        prevState.map((e) => {
+          return {
+            ...e,
+            column: e.id === dragItem.id ? hoverColumn : e.column,
+          }
+        })
+      )
+    )
+    
+  }
+
   const [, drop] = useDrop({
     accept: "Todo",
     canDrop: () => canMoveTodo(title),
     drop: () => ({ title: title }),
+    hover(item, monitor) {
+      const dragColumn = item.currentColumn
+      const hoverColumn = title
+      if (dragColumn === hoverColumn) {
+        return
+      }
+      if (hoverColumn === "In Progress" || item.currentColumn === "In Progress") {
+        return
+      }
+      moveCardColumnHandler(item, hoverColumn)
+
+      item.currentColumn = hoverColumn
+    },
   })
 
   return (
