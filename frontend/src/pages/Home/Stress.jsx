8f78e5374c6ee2c8f9b3a7c2bb05d6d2e60b2e22
haneejo <haandeng@naver.com>
2023-02-12 16:12:17 +0900

    [FE] Feat: 전체 페이지 FONT 및 CSS

--- frontend/src/pages/Home/Stress.jsx
+++ frontend/src/pages/Home/Stress.jsx
@@ -1,17 +1,32 @@
 import styles from "./Stress.module.css"
+import { NavLink } from "react-router-dom"
 import React from "react"
-import startbtn from "../../assets/start-btn.png"
-import mypagebtn from "../../assets/mypage-btn.png"
-import { VscTriangleDown } from "react-icons/vsc"
+import startbtn from "../../assets/last_start.png"
+import mypagebtn from "../../assets/last_mypage.png"
+// import { VscTriangleDown } from "react-icons/vsc"
 
 function Stress(props) {
   return (
     <div ref={props.refVal} className={`${styles["container"]}`}>
       <div className={`${styles["inner"]}`}>
-        <h1>CALM  WAVE 시작하기</h1>
+        <h1>CALM WAVE 시작하기</h1>
         <div className={`${styles["img-wrap"]}`}>
-          <img src={startbtn} alt="임시이미지" />
-          <img src={mypagebtn} alt="임시이미지" />
+          <NavLink to={`/door`} className={`${styles[`shortcut-container`]}`}>
+            {/* <AiFillPlayCircle className={`${styles[`icons`]}`} /> */}
+            <img
+              src={startbtn}
+              alt="룸 입장"
+              className={`${styles[`enter-btn`]}`}
+            />
+          </NavLink>
+
+          <NavLink
+            to={`/profile`}
+            className={`${styles[`shortcut-container`]}`}
+          >
+            {/* <AiFillInfoCircle className={`${styles[`icons`]}`} /> */}
+            <img src={mypagebtn} alt="마이페이지" className={`${styles[`mypage-btn`]}`}/>
+          </NavLink>
         </div>
       </div>
     </div>
