141a11d04dfd4a961f070748301b9c381888ebf7
HanKyeon <hgh21233@gmail.com>
2023-02-13 15:30:59 +0900

    [FE] Feat: Homepage 꾸미기 진행중

--- frontend/src/pages/Home/InfoPage/InfoPage.jsx
+++ frontend/src/pages/Home/InfoPage/InfoPage.jsx
@@ -1,6 +1,6 @@
 import styles from "./InfoPage.module.css"
 import React from "react"
-import doorImg from "../../../assets/doorimg.png"
+import doorImg from "../../../assets/cuteturtle.png"
 import { VscTriangleDown } from "react-icons/vsc"
 
 function InfoPage(props) {
@@ -8,16 +8,21 @@ function InfoPage(props) {
     <div ref={props.refVal} className={`${styles["container"]}`}>
       <div className={`${styles["inner"]}`}>
         <p>
-        최고의 성과를 달성하기 위한 나만의 업무 공간<br/>
-        이제껏 경험 못 했던 체계적이고 편리한 업무 관리 서비스
+          최고의 성과를 달성하기 위한 나만의 업무 공간
+          <br />
+          이제껏 경험 못 했던 체계적이고 편리한 업무 관리 서비스
         </p>
-        <div>
-          <img src={doorImg} alt="룸페이지 이미지" className={`${styles["door-img"]}`}/>
+        <div className={`${styles[`img-container`]}`}>
+          <img
+            src={doorImg}
+            alt="룸페이지 이미지"
+            className={`${styles["door-img"]} ${styles[`info-img`]}`}
+          />
         </div>
       </div>
-        <div className={`${styles["go-down"]}`} onClick={props.goNext}>
+      <div className={`${styles["go-down"]}`} onClick={props.goNext}>
         <VscTriangleDown className={`${styles[`go-down-icon`]}`} />
-        </div>
+      </div>
     </div>
   )
 }
