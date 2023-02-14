947f86d48308742016d4c653853ffd15e15382f9
haneejo <haandeng@naver.com>
2023-02-14 16:16:58 +0900

    [FE] Feat: 모달 및 컨설턴트님 피드백 수정

--- frontend/src/pages/Profile/Info/UserInfo.jsx
+++ frontend/src/pages/Profile/Info/UserInfo.jsx
@@ -1,7 +1,10 @@
 import styles from "./UserInfo.module.css"
 import MyCalendar from "../../../components/MyCalendar/MyCalendar"
 import userlevel from "../../../assets/award.png"
-import { useSelector } from "react-redux"
+import { useSelector,useDispatch } from "react-redux"
+import {useEffect, useState } from "react"
+// import axios from "axios"
+import { AxiosGetUser } from "../../../store/user-slice"
 // import CalendarInfo from "../../../components/MyCalendar/CalendarInfo"
 // import NightSky from "../../../components/Canvas/NightSky/NightSky"
 
@@ -10,6 +13,27 @@ import { useSelector } from "react-redux"
 function UserInfo() {
 
   const userinfo = useSelector(state => state.user);
+  const dispatch = useDispatch();
+  const [isInitial, setIsInitial] = useState(true)
+
+  // useEffect(() => {
+  //   dispatch(AxiosGetUser());
+  // }, [dispatch]);
+
+  // if (!userinfo.userData) {
+  //   return <div>로그인 ㄱㄱ</div>;
+  // }
+
+  useEffect(
+    function () {
+      if (isInitial) {
+        setIsInitial(() => false)
+      }
+      dispatch(AxiosGetUser())
+    },
+    [dispatch, isInitial]
+  )
+
 
   return (
     <div className={`${styles[`routercontainer1`]}`}>
@@ -27,7 +51,7 @@ function UserInfo() {
           <img src={userlevel} alt="" className={`${styles[`my-img`]}`}/>
         </div>
 
-        <p className={`${styles[`welcome-msg`]}`}>Haneejo님, 환영합니다.</p>
+        <p className={`${styles[`welcome-msg`]}`}>{userinfo.userData.nickname}님, 환영합니다.</p>
         </div>
 
       <div className={`${styles["info-Box"]}`}>
