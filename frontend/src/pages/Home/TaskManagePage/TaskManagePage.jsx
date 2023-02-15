7a75bfc9a0bdf1fe8ebf986d00a044883bfbd45b
haneejo <haandeng@naver.com>
2023-02-15 14:55:41 +0900

    [FE] Feat: 버튼 및 모달 수정

--- frontend/src/pages/Home/TaskManagePage/TaskManagePage.jsx
+++ frontend/src/pages/Home/TaskManagePage/TaskManagePage.jsx
@@ -1,6 +1,6 @@
 import styles from "./TaskManagePage.module.css"
 import React from "react"
-import alertimg from "../../../assets/alert.png"
+import alertimg from "../../../assets/bell.png"
 import cuteturtle from "../../../assets/cuteturtle.png"
 import stretching from "../../../assets/stretching.png"
 import angryface from "../../../assets/angryface.png"
