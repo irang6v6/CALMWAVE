f49957ba10bd9834ed0059337573bb0a6e1b7a29
haneejo <haandeng@naver.com>
2023-02-17 10:16:17 +0900

    [FE] Feat: 홈 페이지 애니메이션

--- frontend/src/pages/Home/ResultPage/ResultPage.jsx
+++ frontend/src/pages/Home/ResultPage/ResultPage.jsx
@@ -1,13 +1,12 @@
 import styles from "./ResultPage.module.css"
 import chart1 from "../../../assets/roomresultimg.png"
-import chart2 from "../../../assets/chart2.png"
-import chart3 from "../../../assets/chart3.png"
 import React from "react"
 // import { VscTriangleDown } from "react-icons/vsc"
 
 function ResultPage(props) {
   return (
     <div ref={props.refVal} className={`${styles["container"]}`}>
+      {props.scrollTrigger ? (
       <div className={`${styles["inner"]}`}>
         <p className={`${styles["header-text"]}`}>
           결과페이지에서 확인할 수 있는 다양한 나의 기록
@@ -18,6 +17,7 @@ function ResultPage(props) {
           </div>
         </div>
       </div>
+      ) : null}
     </div>
   )
 }
